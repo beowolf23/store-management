4b5a4d2fe4d9013104f89fb3024a9ff7f484ddab
Mihai Ciocan <mihaiciocan44@gmail.com>
2025-05-28 12:23:23 +0300

    Added a dummy inventory service for stock operations.

---
+++ inventory-service/src/main/java/com/ciocmih/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.ciocmih.inventoryservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
---
+++ inventory-service/src/main/java/com/ciocmih/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,15 @@
+package com.ciocmih.inventoryservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    @GetMapping
+    public String getInventory() {
+        return "Inventory data";
+    }
+}
